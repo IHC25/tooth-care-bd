603479f64f8c10c2fee286bf3a228db6108adcbc
arefin6 <imranhchy@gmail.com>
2022-04-17 06:26:08 +0600

    Email Password based authentication implemented

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,27 +1,75 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, Form, Spinner } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../SocialLogin/SocialLogin";
+import auth from "../../firebase.init";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 
 const SignUp = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  if (loading) {
+    return (
+      <div className="w-100 d-flex justify-content-center align-item-center">
+        <Spinner animation="border" variant="primary"></Spinner>
+      </div>
+    );
+  }
+
+  if (error) {
+    setErrorMessage(error?.message);
+  }
+
+  if (user) {
+    navigate("/home");
+  }
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const confirmPassword = event.target.confirmPassword.value;
+    console.log(confirmPassword);
+    if (password === confirmPassword) {
+      createUserWithEmailAndPassword(email, password);
+    } else {
+      setErrorMessage("Password Not Matched");
+    }
+  };
   return (
     <div className="container w-50 mx-auto my-5">
       <h2>Sign Up</h2>
-      <Form>
+      <Form onSubmit={handleSignUp}>
         <Form.Group className="mb-3" controlId="formBasicText">
           <Form.Control type="text" placeholder="Enter Your Name" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" required />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control
+            name="email"
+            type="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            name="confirmPassword"
             type="password"
             placeholder="Confirm Password"
             required
@@ -36,6 +84,9 @@ const SignUp = () => {
           Sign Up
         </Button>
       </Form>
+      <div>
+        <p className="text-danger">{errorMessage}</p>
+      </div>
       <p>
         Already Have an Account?
         <Link
